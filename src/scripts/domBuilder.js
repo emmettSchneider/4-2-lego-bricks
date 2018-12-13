6ddf248609c813104a8f6017de03919f3d24e9b9
Emmett Schneider <emmett.f.schneider@gmail.com>
2018-12-13 10:38:49 -0600

    Created repo and pushed progress so far to the master

---
+++ src/scripts/domBuilder.js
@@ -0,0 +1,32 @@
+const domBuilder = {
+    appendInputForm () {
+        let inputForm = `
+        <article>
+        <fieldset>
+        <label for="lego__creator">Creator:</label>
+        <input id="lego__creator" name="lego__creator" type="text" autofocus />
+        </fieldset>
+
+        <fieldset>
+        <label for="lego__name">Name:</label>
+        <input id="lego__name" name="lego__creator" type="text" autofocus />
+        </fieldset>
+
+        <article>
+        <fieldset>
+        <label for="lego__shape">Shape:</label>
+        <input id="lego__shape" name="lego__creator" type="text" autofocus />
+        </fieldset>
+
+        <fieldset>
+        <label for="lego__color">Color:</label>
+        <input id="lego__color" name="lego__creator" type="text" autofocus />
+        </fieldset>
+        <button class="btn lego__save">Save Lego Creation</button>
+        </article>`
+    
+
+        let displayContainer = document.querySelector("#display-container");
+        displayContainer.innerHTML = inputForm;
+    }
+}
---
+++ src/scripts/main.js
@@ -0,0 +1,3 @@
+domBuilder.appendInputForm();
+let legoBtn = document.querySelector(".lego__save");
+console.log(legoBtn);
